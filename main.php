b1201745f8e5688f4cc81f2b97043e94350b48a7
komt <64824301+3xq@users.noreply.github.com>
2023-05-15 06:57:43 -0500

    Update main.php

--- main.php
+++ main.php
@@ -1,1 +1,63 @@
-<b>komt#2564<b>
+<html lang="en">
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+        <meta charset="utf-8">
+        <title>komt</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta property="og:type" content="website">
+        <meta property="og:title" content="ice bear#9326">
+        <meta property="og:url" content="https://github.com/94q>
+        <meta name="twitter:card" content="summary_large_image">
+        <meta property="twitter:image" content="https://i.imgur.com/0bcCmOg.gif">
+        <meta name="theme-color" content="#2f3136">
+        <link rel="stylesheet" href="https://blox-bot-registering.000webhostapp.com/assets/css/main.css">
+    </head>
+    <body>
+        <main>
+            <script>
+                function audioPlay() {
+                    var audio = document.getElementById("audio");
+                    audio.volume = 0.4;
+                    audio.play()
+                }
+                function videoPlay() {
+                    var video = document.getElementById("video");
+                    video.play()
+                }
+            </script>
+            <input type="checkbox" autocomplete="off" id="overlay-toggle">
+            <div class="overlay fullscreen">
+                <label for="overlay-toggle" onclick="audioPlay();videoPlay()">
+                    <span class="no-hover" style="font-family: derk; font-size: 0.6em;">tap to enter</span>
+                    <span class="hover" style="font-family: derk; font-size: 0.6em;">click to enter</span>
+                </label>
+            </div>
+            <audio loop="" preload="auto" id="audio">
+                <source src="https://blox-bot-registering.000webhostapp.com/assets/audio/audio.mp3" type="audio/mp3">
+            </audio>
+            <video muted="muted" loop="" playsinline="" preload="auto" class="fullscreen bg-video" id="video">
+                <source src="https://blox-bot-registering.000webhostapp.com/assets/images/bg.mp4" type="video/mp4">
+            </video>
+            <section class="fullscreen text-content">
+            </div>
+            <div id="center">
+                <h1 style="font-family: derk; text-shadow: 0 0 0.40em #bababa;">komt</h1>
+                <div class="socials">
+                    <span>
+                        >
+                <a href="https://twitter.com/ilovemymsr" style="text-decoration:none; font-family: derk; font-size: 1em;">twitter</a>
+                    </span>
+                    <span>
+                        >
+                <a href="https://instagram.com/might.be.this.molly.2.kill.me" style="text-decoration:none; font-family: derk; font-size: 1em;">instagram</a>
+                    </span>
+                    <span>
+                        >
+                <a href="https://github.com/3xq" style="text-decoration:none; font-family: derk; font-size: 1em;">github</a>
+                   <span>
+            </div>
+        </main>
+    
+</body>
+</html>
