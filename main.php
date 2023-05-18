989cdc335fb26eb6ec48814810f519b949897343
komt <64824301+3xq@users.noreply.github.com>
2023-05-18 17:08:10 -0500

    Update main.php

--- main.php
+++ main.php
@@ -1,3 +1,9 @@
 <?php
-mkdir("u");
+$myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+$txt = "John Doe\n";
+fwrite($myfile, $txt);
+$txt = "Jane Doe\n";
+fwrite($myfile, $txt);
+fclose($myfile);
+echo "made file newfile.txt"
 ?>
