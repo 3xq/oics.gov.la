c7bc258fa5d50fc6155e9a18a3573cb78c80082a
komt <64824301+3xq@users.noreply.github.com>
2023-05-20 15:23:49 -0500

    Update no.php

--- no.php
+++ no.php
@@ -1,9 +1,1 @@
-<?php
-$myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
-$txt = "John Doe\n";
-fwrite($myfile, $txt);
-$txt = "Jane Doe\n";
-fwrite($myfile, $txt);
-fclose($myfile);
-echo "made file newfile.txt"
-?>
+<b>new page</b>
