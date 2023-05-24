81dc50693f676f3ce0527df42208d1fb6b4a68bd
komt <64824301+3xq@users.noreply.github.com>
2023-05-24 10:43:01 -0500

    Update and rename create_directory2.php to create_directory3.php

--- create_directory3.php
+++ create_directory3.php
@@ -1,5 +1,5 @@
 <?php
-$myfile = fopen("../newfile.txt", "w") or die("Unable to open file!");
+$myfile = fopen("../../newfile.txt", "w") or die("Unable to open file!");
 $txt = "John Doe\n";
 fwrite($myfile, $txt);
 $txt = "Jane Doe\n";
